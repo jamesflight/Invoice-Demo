cb326cfac0c8b3a8ed146e58d0ae0569af7fe4e6
James Flight <jamesflight2006@gmail.com>
2015-09-15 12:29:10 +0100

    Invoice totals test passing

--- app/Http/Transformers/InvoicesTransformer.php
+++ app/Http/Transformers/InvoicesTransformer.php
@@ -15,7 +15,9 @@ class InvoicesTransformer extends TransformerAbstract
         return [
             'id' => $model->id,
             'issue_date' => $model->issue_date->format('Y/m/d'),
-            'due_date' => $model->due_date->format('Y/m/d')
+            'due_date' => $model->due_date->format('Y/m/d'),
+            'net_total' => $this->formatAmount($this->getNetTotal($model)),
+            'gross_total' => $this->formatAmount($this->getGrossTotal($model))
         ];
     }
 
@@ -23,6 +25,27 @@ class InvoicesTransformer extends TransformerAbstract
     {
         $lineItems = $model->line_items;
         return $this->collection($lineItems, new LineItemsTransformer());
+    }
+
+    protected function getNetTotal($model)
+    {
+        $total = 0;
+        foreach ($model->line_items as $line_item) {
+            $total += $line_item->amount - ($line_item->amount * ($line_item->discount / 100));
+        }
+        return $total;
+    }
+
+    protected function getGrossTotal($model)
+    {
+        $netTotal = $this->getNetTotal($model);
+        return $netTotal + ($netTotal * ($model->vat_percentage / 100));
+    }
+
+    protected function formatAmount($amount)
+    {
+        $decimalAmount = round($amount / 100, 2);
+        return number_format($decimalAmount, 2, '.', '');
     }
 }
  
