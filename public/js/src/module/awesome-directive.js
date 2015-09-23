a9610a2be22bb720433510dab08e9c2a929a838b
James Flight <jamesflight2006@gmail.com>
2015-09-23 15:25:32 +0100

    update

--- public/js/src/module/awesome-directive.js
+++ public/js/src/module/awesome-directive.js
@@ -4,20 +4,6 @@ angular.module('invoicio.awesome-module.awesome-directive', [])
 
 .directive('awesomeDirective', function ($interval) {
     return {
-        template:"<div>An awesome directive is what I am.</div>",
-        compile:function () {
-            console.log('tet');
-
-            return {
-                post:function (scope) {
-                    scope.count = 0;
-                    console.log(scope.count);
-
-                    $interval(function () {
-                        scope.count = scope.count++;
-                    }, 1000);
-                }
-            }
-        }
+        template:"<div>An awesome directive is what I am.</div>"
     };
 });
